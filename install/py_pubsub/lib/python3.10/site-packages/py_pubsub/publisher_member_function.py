dc3436b29c64771396a9850c05ad0bb59783ac27
Vikas Rachakonda <vr20aao@herts.ac.uk>
2024-02-24 23:52:56 +0530

    first commit

---
+++ install/py_pubsub/lib/python3.10/site-packages/py_pubsub/publisher_member_function.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import time
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        timer_period = 1.0
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: {}'.format(self.i)
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    talker = Talker()
+    rclpy.spin(talker)
+    talker.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
